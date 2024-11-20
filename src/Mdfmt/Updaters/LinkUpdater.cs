2160ef0e3d9963e68fb9939ed2512ecbdd8d24d2
Steinar Flatland <steinar.flatland@gmail.com>
2024-11-19 20:44:27 -0500

    Created an Updaters namespace and made the updater modular.

---
+++ src/Mdfmt/Updaters/LinkUpdater.cs
@@ -0,0 +1,45 @@
+﻿using Mdfmt.Generators.Links;
+using Mdfmt.Model;
+using Mdfmt.Options;
+using System;
+
+namespace Mdfmt.Updaters;
+
+public class LinkUpdater(CommandLineOptions options, ILinkDestinationGenerator linkDestinationGenerator) : UpdaterBase(options)
+{
+    private readonly ILinkDestinationGenerator _linkDestinationGenerator = linkDestinationGenerator;
+
+    public override void Update(MdStruct md)
+    {
+        foreach (LinkRegion linkRegion in md.LinkRegions)
+        {
+            bool linkIsWithinSameFile =
+                string.IsNullOrEmpty(linkRegion.Destination) ||
+                linkRegion.Destination.StartsWith('#') ||
+                linkRegion.Destination.StartsWith($"./{md.FileName}");
+            if (linkIsWithinSameFile)
+            {
+                if (md.TryGetHeadingRegion(linkRegion.Label, out HeadingRegion headingRegion) ||
+                    md.TryGetHeadingRegion(linkRegion.Destination, out headingRegion))
+                {
+                    string destination = _linkDestinationGenerator.GenerateLinkDestination(md.FileName, headingRegion.HeadingText);
+                    if (linkRegion.Destination != destination)
+                    {
+                        linkRegion.Destination = destination;
+                        if (_options.Verbose)
+                        {
+                            Console.WriteLine($"  Updated link with label [{linkRegion.Label}] to target destination ({linkRegion.Destination})");
+                        }
+                    }
+                }
+                else
+                {
+                    if (_options.Verbose)
+                    {
+                        Console.WriteLine($"  Could not match link to heading: {linkRegion.Content}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Mdfmt/Updaters/TocUpdater.cs
@@ -0,0 +1,54 @@
+﻿using Mdfmt.Generators.Tocs;
+using Mdfmt.Model;
+using Mdfmt.Options;
+using System;
+
+namespace Mdfmt.Updaters;
+
+public class TocUpdater(CommandLineOptions options, TocGenerator tocGenerator) : UpdaterBase(options)
+{
+    private readonly TocGenerator _tocGenerator = tocGenerator;
+
+    public override void Update(MdStruct md)
+    {
+        // Generate a TOC based on headings.
+        Toc newToc = _tocGenerator.GenerateToc(md.FileName, md.HeadingRegions, md.Newline);
+
+        // Is the desired end state to have a TOC with the content of newToc in the document?
+        bool tocShouldExist =
+            _options.MinimumEntryCount > 0 &&
+            newToc.EntryCount >= _options.MinimumEntryCount;
+
+        if (md.HasToc) // The document has a TOC already.
+        {
+            if (tocShouldExist)
+            {
+                Region tocRegion = md.TocRegion;
+                if (tocRegion.Content != newToc.Content)
+                {
+                    tocRegion.Content = newToc.Content;
+                    if (_options.Verbose)
+                    {
+                        Console.WriteLine("  Updated TOC");
+                    }
+                }
+            }
+            else
+            {
+                md.DeleteToc();
+                Console.WriteLine("  Removed TOC");
+            }
+        }
+        else // The document does not have a TOC.
+        {
+            if (tocShouldExist)
+            {
+                md.AddToc(newToc.Content);
+                if (_options.Verbose)
+                {
+                    Console.WriteLine("  Inserted new TOC");
+                }
+            }
+        }
+    }
+}
