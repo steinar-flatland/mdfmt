d9a0c5d7bbd33208d2828d3ec3c4b52516af4fd5
Steinar Flatland <steinar.flatland@gmail.com>
2024-11-20 21:27:34 -0500

    Added fluent validation for command line options

--- src/Mdfmt/Options/CommandLineOptions.cs
+++ src/Mdfmt/Options/CommandLineOptions.cs
@@ -10,6 +10,12 @@ public class CommandLineOptions
     [Option('n', "heading-numbers", Default = "1.", HelpText = "Whether to include heading numbers.  One of: [none, 1., 1].  Use 1. or 1 to include heading numbers. The name of the \"1\" options indicates whether generated numbers will end in a period.")]
     public string HeadingNumbering { get; set; }
 
+    [Option("minimum-entry-count", Default = 3, HelpText = "The minimum number of entries for which to include TOC.  0 turns off/removes TOC.")]
+    public int MinimumEntryCount { get; set; }
+
+    [Option("newline-strategy", Default = NewlineStrategy.PreferWindows, HelpText = "One of: [Unix, Windows, PreferUnix, PreferWindows].  Preferred options respect the file and take effect only if the file has a mixture.")]
+    public NewlineStrategy NewlineStrategy { get; set; }
+
     [Option('r', "recursive", Default = false, HelpText = "Process .md files recursively in all subfolders.")]
     public bool Recursive { get; set; }
 
@@ -18,10 +24,4 @@ public class CommandLineOptions
 
     [Value(0, Default = ".", HelpText = "The directory in which to process .md files, or a specific .md file.")]
     public string Path { get; set; }
-
-    [Option("minimum-entry-count", Default = 3, HelpText = "The minimum number of entries for which to include TOC.  0 turns off/removes TOC.")]
-    public int MinimumEntryCount { get; set; }
-
-    [Option("newline-strategy", Default = NewlineStrategy.PreferWindows, HelpText = "One of: [Unix, Windows, PreferUnix, PreferWindows].  Preferred options respect the file and take effect only if the file has a mixture.")]
-    public NewlineStrategy NewlineStrategy { get; set; }
 }
