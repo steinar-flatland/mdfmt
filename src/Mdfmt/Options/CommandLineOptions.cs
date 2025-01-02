41efc805e96ba8809c8b8c3fe562a61db09158cc
Steinar Flatland <steinar-flatland@users.noreply.github.com>
2025-01-01 21:34:36 -0600

    non-opinionated newline management (#23)

--- src/Mdfmt/Options/CommandLineOptions.cs
+++ src/Mdfmt/Options/CommandLineOptions.cs
@@ -10,14 +10,14 @@ internal class CommandLineOptions
     [Option('f', "flavor", Default = Flavor.Common, HelpText = "Flavor of Markdown formatting.  One of: [Common, Azure].")]
     public Flavor Flavor { get; set; }
 
-    [Option('h', "heading-numbers", HelpText = "Whether to include heading numbers.  If option omitted, no changes are made to heading numbers.  When specified, one of: [none, 1., 1].  Use none to remove heading numbers.  Use 1. or 1 to include heading numbers either with or without a trailing period.")]
+    [Option('h', "heading-numbers", HelpText = "Whether to include heading numbers.  When specified, one of: [none, 1., 1].  Use none to remove heading numbers.  Use 1. or 1 to include heading numbers either with or without a trailing period.  If option omitted, no changes to heading numbers.")]
     public string HeadingNumbering { get; set; }
 
     [Option('t', "toc-threshold", Default = 3, HelpText = "The minimum number of entries for which to include TOC.  0 turns off/removes TOC.")]
     public int TocThreshold { get; set; }
 
-    [Option("newline-strategy", Default = NewlineStrategy.PreferWindows, HelpText = "One of: [Unix, Windows, PreferUnix, PreferWindows].  Preferred options respect the file and take effect only if the file has a mixture.")]
-    public NewlineStrategy NewlineStrategy { get; set; }
+    [Option("newline-strategy", HelpText = "One of: [Unix, Windows, PreferUnix, PreferWindows].  Preferred options do nothing unless file has mixture of newline styles.  If option omitted, no changes to existing newlines, and any new newlines introduced by Mdfmt follow the predominant style of the current file.")]
+    public NewlineStrategy? NewlineStrategy { get; set; }
 
     [Option('r', "recursive", Default = false, HelpText = "Process .md files recursively in all subfolders.")]
     public bool Recursive { get; set; }
--- src/Mdfmt/Options/CommandLineOptionsValidator.cs
+++ src/Mdfmt/Options/CommandLineOptionsValidator.cs
@@ -10,7 +10,7 @@ internal class CommandLineOptionsValidator : AbstractValidator<CommandLineOption
         RuleFor(o => o.HeadingNumbering).Must(o => o == null || HeadingNumbering.Options.Contains(o.ToLower())).
             WithMessage($"Valid options for -h and --heading-numbers: [{string.Join(',', HeadingNumbering.Options)}]");
         RuleFor(o => o.TocThreshold).GreaterThanOrEqualTo(0);
-        RuleFor(o => o.NewlineStrategy).IsInEnum();
+        RuleFor(o => o.NewlineStrategy).IsInEnum().Unless(o => o == null);
         RuleFor(o => o.Path).NotEmpty();
     }
 }
